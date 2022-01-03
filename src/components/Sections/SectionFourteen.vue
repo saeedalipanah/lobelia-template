5d7fc2fb7556f53e18887cfbba5767baf142d2d4
saeedalipanah <saeedalipanah1374@gmail.com>
2022-01-03 15:54:38 +0330

    section fourteen added

--- src/components/Sections/SectionFourteen.vue
+++ src/components/Sections/SectionFourteen.vue
@@ -1,34 +1,72 @@
 <template>
-  <div class="contanier blue lighten-2">
+  <div class="container">
     <div class="title">
-      <h1>THE CRYPTO SOLUTION</h1>
-      <p>
-        The Cryptocurrency industry is one of the Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </p>
+      <h1>CONTACT ICO CRYPTO</h1>
+      <p>Any question? Reach out to us and we’ll get back to you shortly.</p>
     </div>
     <div class="content">
       <v-container>
-        <v-row>
-          <v-col >
+        <v-row justify="center">
+          <v-col cols="12" sm="4" v-for="(item,i) in items" :key="i">
+            <div class=" red d-flex justify-center align-center">
+              <span class="fas fa-phone"></span>
+              <h4 class="mx-4">{{item.text}}</h4>
+            </div>
           </v-col>
         </v-row>
       </v-container>
+      <v-container>
+        <v-row class="">
+          <v-col cols="12" class=" red">
+              <v-text-field class="input-text"
+            label="Your Name"
+          ></v-text-field>
+          </v-col>
+          <v-col cols="12">
+              <v-text-field
+              class="input-text"
+            label="Your Email"
+          ></v-text-field>
+          </v-col>
+          <v-col cols="12">
+              <v-text-field class="input-text"
+            label="Your Message"
+          ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+      <div class="d-flex justify-center">
+      <v-btn rounded>submit</v-btn>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      
-    }
-  }
+  data() {
+    return {
+      items: [
+        {
+          icon: "",
+          text: "+44 0123 4567",
+        },
+        {
+          icon: "",
+          text: "info@company.com",
+        },
+        {
+          icon: "",
+          text: "Join us on Telegram",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-.contanier {
+.container {
   .title {
     display: flex;
     flex-direction: column;
@@ -36,8 +74,21 @@ export default {
     text-align: center;
   }
   .content {
-  
+   .input-text {
+     width: 60%;
+     margin: 0 auto;
+     ::v-deep .v-label{
+       left: 45% !important;
+     }
   }
 }
-
+}
+span{
+  font-size: 30px;
+  position: relative;
+  background: rgb(255, 253, 253);
+  padding: 10px;
+  border-radius: 50%;
+  
+}
 </style>
