7c2484a3b39eea05dd6f02bb4042f5eaa65dfb3f
saeedalipanah <saeedalipanah1374@gmail.com>
2024-01-24 17:07:23 +0330

    pwa added

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import './assets/style/global.scss'
 import VueParticles from 'vue-particles'
+import './registerServiceWorker'
 Vue.use(VueParticles)
 
 Vue.config.productionTip = false
